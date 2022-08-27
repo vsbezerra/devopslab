8deceab99309a5bcf0ff97e3b33478cf00c4a36d
vsbezerra <94020737+vsbezerra@users.noreply.github.com>
2022-08-27 10:42:04 -0300

    Added: Docker File

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Gaby acho que deu bom aqui!!"
+    return "Gaby acho que deu bom aqui!! To no Docker"
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
